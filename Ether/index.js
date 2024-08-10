2469c9126826180e010bcf480b2963f46d440fd8
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-08-10 17:06:48 +0530

    add mnemonic generator

--- Ether/index.js
+++ Ether/index.js
@@ -1,33 +1,26 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+// import { ethers } from "ethers";
 Object.defineProperty(exports, "__esModule", { value: true });
-const ethers_1 = require("ethers");
-function signAndVerify() {
-    return __awaiter(this, void 0, void 0, function* () {
-        // Generate a random wallet
-        const wallet = ethers_1.ethers.Wallet.createRandom();
-        // Extract the public and private keys
-        const publicKey = wallet.address;
-        const privateKey = wallet.privateKey;
-        console.log("Public Key (Address):", publicKey);
-        console.log("Private Key:", privateKey);
-        // Message to sign
-        const message = "hello world";
-        // Sign the message using the wallet's private key
-        const signature = yield wallet.signMessage(message);
-        console.log("Signature:", signature);
-        // Verify the signature
-        const recoveredAddress = ethers_1.ethers.verifyMessage(message, signature);
-        console.log("Recovered Address:", recoveredAddress);
-        console.log("Signature is valid:", recoveredAddress === publicKey);
-    });
-}
-signAndVerify();
+// async function signAndVerify() {
+//     // Generate a random wallet
+//     const wallet = ethers.Wallet.createRandom();
+//     // Extract the public and private keys
+//     const publicKey = wallet.address;
+//     const privateKey = wallet.privateKey;
+//     console.log("Public Key (Address):", publicKey);
+//     console.log("Private Key:", privateKey);
+//     // Message to sign
+//     const message = "hello world";
+//     // Sign the message using the wallet's private key
+//     const signature = await wallet.signMessage(message);
+//     console.log("Signature:", signature);
+//     // Verify the signature
+//     const recoveredAddress = ethers.verifyMessage(message, signature);
+//     console.log("Recovered Address:", recoveredAddress);
+//     console.log("Signature is valid:", recoveredAddress === publicKey);
+// }
+// signAndVerify();
+const bip39_1 = require("bip39");
+// Generate a 12-word mnemonic
+const mnemonic = (0, bip39_1.generateMnemonic)();
+console.log('Generated Mnemonic:', mnemonic);
--- Ether/index.ts
+++ Ether/index.ts
@@ -1,28 +1,34 @@
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
 
-async function signAndVerify() {
+// async function signAndVerify() {
 
-    // Generate a random wallet
-    const wallet = ethers.Wallet.createRandom();
+//     // Generate a random wallet
+//     const wallet = ethers.Wallet.createRandom();
 
-    // Extract the public and private keys
-    const publicKey = wallet.address;
-    const privateKey = wallet.privateKey;
+//     // Extract the public and private keys
+//     const publicKey = wallet.address;
+//     const privateKey = wallet.privateKey;
 
-    console.log("Public Key (Address):", publicKey);
-    console.log("Private Key:", privateKey);
+//     console.log("Public Key (Address):", publicKey);
+//     console.log("Private Key:", privateKey);
 
-    // Message to sign
-    const message = "hello world";
+//     // Message to sign
+//     const message = "hello world";
 
-    // Sign the message using the wallet's private key
-    const signature = await wallet.signMessage(message);
-    console.log("Signature:", signature);
+//     // Sign the message using the wallet's private key
+//     const signature = await wallet.signMessage(message);
+//     console.log("Signature:", signature);
 
-    // Verify the signature
-    const recoveredAddress = ethers.verifyMessage(message, signature);
+//     // Verify the signature
+//     const recoveredAddress = ethers.verifyMessage(message, signature);
 
-    console.log("Recovered Address:", recoveredAddress);
-    console.log("Signature is valid:", recoveredAddress === publicKey);
-}
-signAndVerify();
+//     console.log("Recovered Address:", recoveredAddress);
+//     console.log("Signature is valid:", recoveredAddress === publicKey);
+// }
+// signAndVerify();
+
+import { generateMnemonic } from 'bip39';
+
+// Generate a 12-word mnemonic
+const mnemonic = generateMnemonic();
+console.log('Generated Mnemonic:', mnemonic);
