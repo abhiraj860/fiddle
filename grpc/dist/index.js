3af69d9a07bfff4b9b7e466acf196964b6cc0c9f
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-08-10 11:06:58 +0530

    add proto

--- grpc/dist/index.js
+++ grpc/dist/index.js
@@ -1,19 +1,16 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const rpc_1 = require("./rpc");
-function toDocall() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const todos = yield (0, rpc_1.getTodos)();
-        console.log(todos);
-    });
-}
-toDocall();
+const protobufjs_1 = __importDefault(require("protobufjs"));
+protobufjs_1.default.load('a.proto').then(root => {
+    const Person = root.lookupType('Person');
+    const person = { name: "Abhiraj", age: 30 };
+    const buffer = Person.encode(person).finish();
+    require('fs').writeFileSync('person.bin', buffer);
+    console.log('Person is serialized and save to bin ');
+    const data = require('fs').readFileSync('person.bin');
+    const deserialisation = Person.decode(data);
+    console.log("Person deserialised from bin ", deserialisation);
+}).catch(console.error);
--- grpc/src/index.ts
+++ grpc/src/index.ts
@@ -1,11 +1,14 @@
-import { getTodos } from "./rpc";
+import protobuf from 'protobufjs';
 
+protobuf.load('a.proto').then(root=>{
+    const Person = root.lookupType('Person');
+    const person = {name: "Abhiraj", age: 30};
+    const buffer = Person.encode(person).finish();
+    require('fs').writeFileSync('person.bin', buffer);
 
-async function toDocall() {
-    const todos = await getTodos();
-    console.log(todos);
-}
-
-toDocall();
-
-
+    console.log('Person is serialized and save to bin ');
+    
+    const data = require('fs').readFileSync('person.bin');
+    const deserialisation = Person.decode(data);
+    console.log("Person deserialised from bin ", deserialisation);
+}).catch(console.error);
