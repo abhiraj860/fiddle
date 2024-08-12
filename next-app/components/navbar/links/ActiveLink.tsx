a2323f8efeb36ac68ca8fd29ed985d8f0b4ba872
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-08-12 17:14:12 +0530

    navbar done

--- next-app/components/navbar/links/ActiveLink.tsx
+++ next-app/components/navbar/links/ActiveLink.tsx
@@ -22,14 +22,58 @@ export default function ActiveLink() {
 			link: "/blog",
 		},
 	];
-    const getPath = usePathname();
+	const session = true;
+	const isAdmin = true;
+
+	const getPath = usePathname();
 	return (
 		<div>
 			{links.map((value) => (
-				<Link href={value.link} key={value.title} className={` font-semibold mr-8 ${(getPath === value.link) && 'bg-textcolor text-bgcolor p-2 px-3 rounded-full'}`}>
+				<Link
+					href={value.link}
+					key={value.title}
+					className={` font-semibold mr-8 ${
+						getPath === value.link &&
+						"bg-textcolor text-bgcolor p-2 px-5 rounded-full"
+					}`}
+				>
 					{value.title}
 				</Link>
 			))}
+			{session ? (
+				<>
+					{isAdmin && (
+						<Link
+							href={"/admin"}
+							className={` font-semibold mr-8 ${
+								getPath === "/admin" &&
+								"bg-textcolor text-bgcolor p-2 px-5 rounded-full"
+							}`}
+						>
+							Admin
+						</Link>
+					)}
+				<button className="bg-textcolor text-bgcolor px-5 p-2 font-semibold rounded-md">
+					Logout
+				</button>
+
+				</>
+			) : (
+				<button className="bg-textcolor text-bgcolor px-5 p-2 font-semibold rounded-md">
+					Login
+				</button>
+			)}
 		</div>
 	);
 }
+
+// 		<Link
+// 			href={"/admin"}
+// 			className={` font-semibold mr-8 ${
+// 				(getPath === "/admin") &&
+// 				"bg-textcolor text-bgcolor p-2 px-5 rounded-full"
+// 			}`}
+// 		>
+// 			Admin
+// 		</Link>
+// 	)
