bd7c35eb97b94e483278238fb75be28d0d457744
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-08-16 23:15:04 +0530

    arrange

--- next-app/components/footer/Footer.tsx
+++ next-app/components/footer/Footer.tsx
@@ -3,8 +3,12 @@ import Link from "next/link";
 export default function Footer() {
 	return (
 		<div className="flex justify-between py-6">
-			<div className="text-grey text-xs font-bold"><Link href="/">LamaDev</Link></div>
-			<div className="text-xs text-grey font-bold">Lama creative thoughts agency ©️ All rights reserved</div>
+			<div className="text-grey text-xs font-bold">
+				<Link href="/">LamaDev</Link>
+			</div>
+			<div className="text-xs text-grey font-bold">
+				Lama creative thoughts agency ©️ All rights reserved
+			</div>
 		</div>
 	);
 }
